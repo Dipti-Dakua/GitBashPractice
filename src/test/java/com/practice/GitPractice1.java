4756f0ad63d969d3ab0b38938ac285604a079928
Dipti-Dakua <diptidakua2@gmail.com>
2023-03-09 17:51:46 +0530

    1st changes done

--- src/test/java/com/practice/GitPractice1.java
+++ src/test/java/com/practice/GitPractice1.java
@@ -3,6 +3,7 @@ package com.practice;
 public class GitPractice1 {
 	public static void main(String[] args) {
 		System.out.println("git practice 1");
+		System.out.println("git practice running");
 	}
 
 }
