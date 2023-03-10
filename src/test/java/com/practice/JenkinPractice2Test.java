bd2af69bfd92b3bf1124d9ea48dafeec9552b630
Dipti-Dakua <diptidakua2@gmail.com>
2023-03-10 15:08:55 +0530

    jenkin practice

---
+++ src/test/java/com/practice/JenkinPractice2Test.java
@@ -0,0 +1,13 @@
+package com.practice;
+
+import org.testng.annotations.Test;
+
+public class JenkinPractice2Test {
+	@Test
+	public void jenkinPractice2()
+	{
+		System.out.println("test3");
+		System.out.println("test4");
+	}
+
+}
---
+++ src/test/java/com/practice/JenkinPracticeTest.java
@@ -0,0 +1,16 @@
+package com.practice;
+
+import org.testng.annotations.Test;
+
+public class JenkinPracticeTest {
+	
+@Test
+public void jenkinPractice()
+{
+	System.out.println("test1");
+	System.out.println("test2");
+}
+
+
+}
+
